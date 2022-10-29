2b7ebc425e63cf337b5121d5458d1be7502e1aa1
Thomas Bnt <thomasbnt@protonmail.com>
2022-10-30 01:07:38 +0200

    if no commits, skip

--- src/discord.js
+++ src/discord.js
@@ -6,8 +6,9 @@ module.exports.send = (id, token, repo, branch, url, commits, size, in_thread) =
         let client
         console.log('Preparing Webhook...')
         try {
-            if (repo === []) {
-                return 
+            if (!commits) {
+                console.log('No commits, skipping...')
+                return resolve(true)
             }
             console.log(in_thread)
             if (in_thread) {
